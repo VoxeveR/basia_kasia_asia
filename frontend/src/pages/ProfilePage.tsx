f17d20ab6001f22abaefaa61a79b71dca88a7fff
VoxeveR <piotrzietek01@gmail.com>
2025-10-26 16:51:21 +0100

    xd

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,14 +1,17 @@
 import Menu from "@/components/Menu";
 
+const dummyDate="28-11-2001";
+
 function ProfilePage() {
     return (
         <div className='w-full h-full flex flex-col gap-4 p-4'>
             <Menu />
-            <div>
-            <div>Profile Page</div>
+            <div className="justify-center items-center flex flex-col gap-4">
+                <div className="text-5xl bold">Profile Page</div>
+                <div>Joined: {dummyDate}</div>
+                <img src="/path/to/profile-pic.jpg" alt="Profile Picture" className="rounded-full w-32 h-32" />
+                
             </div>
-
-
         </div>
     )
 }
