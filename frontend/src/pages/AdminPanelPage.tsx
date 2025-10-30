c77747c72eaa401e6f2ffb466bcb81da158f7b76
odlugiewicz <odlugiewicz.9@gmail.com>
2025-10-30 13:09:53 +0100

    added admin panel page

---
+++ frontend/src/pages/AdminPanelPage.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/Foooter";
+import Menu from "@/components/Menu";
+
+function AdminPanelPage() {
+    return (
+        <div className='w-full h-full flex flex-col gap-4 p-4'> 
+            <Menu />
+            
+
+
+            <Footer  />
+        </div>
+    )
+}
+
+export default AdminPanelPage;
